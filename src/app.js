78bc55dbc100a2e96ca4f01195647e58a983e87e
Nanda-Souza <fesouoli@gmail.com>
2023-01-07 03:22:53 -0300

    POST /tweets UNAUTHORIZED

--- src/app.js
+++ src/app.js
@@ -7,17 +7,43 @@ const server = express()
 server.use(cors())
 server.use(express.json());
 
-const users = []
+const users = [
+    {
+        username: "lavitz",
+        avatar: "https://static.wikia.nocookie.net/legendofdragoon/images/2/25/Lavits.jpg"   
+    }]
+const tweets = [
+    {
+        username: "lavitz",
+        avatar: "https://static.wikia.nocookie.net/legendofdragoon/images/2/25/Lavits.jpg",
+        tweet: "eitaa"   
+    }]
 
 server.get("/status", (req, res) => {
     res.send("Local Server is Running")
 })
 
+server.get("/tweets", (req, res) => {
+    res.send(tweets)
+})
+
+server.post("/tweets", (req, res) => {
+    const { username, tweet } = req.body
+
+    const newTweet = users.find(u => u.username === username)
+
+    if(!newTweet){
+        return res.send("UNAUTHORIZED")
+    }
+    
+    res.send("OK")
+})
+
+
 server.post("/sign-up", (req, res) => {
     const newUser = req.body
     users.push(newUser)
-    console.log(users)
-  
+      
     res.send("OK")
   })
 
