fb151639aa7f73b35106f95a7a497120002ceba1
Nanda-Souza <fesouoli@gmail.com>
2023-01-07 02:37:22 -0300

    POST /sign-up

--- src/app.js
+++ src/app.js
@@ -3,11 +3,23 @@ import cors from "cors"
 
 const server = express()
 
+
 server.use(cors())
+server.use(express.json());
+
+const users = []
 
 server.get("/status", (req, res) => {
     res.send("Local Server is Running")
 })
+
+server.post("/sign-up", (req, res) => {
+    const newUser = req.body
+    users.push(newUser)
+    console.log(users)
+  
+    res.send("OK")
+  })
 
 server.listen(5000, () => {
   console.log('Server is up!!!')
